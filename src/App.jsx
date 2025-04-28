0b0a84e41be121a7df4900d3ff2dfff0517332b1
Android Studio <devandroid@ort>
2025-04-28 14:08:07 -0300

    Listado listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Formulario from './components/Formulario'
-import Cita from './components/Cita'
+import Listado from './components/Listado'
 
 function App() {
   return (
@@ -9,12 +9,7 @@ function App() {
       <div className="container">
         <div className="row">
           <Formulario />
-          <div className="one-half column">
-            <h2>Administra tus citas</h2>
-            <Cita mascota="doggo" dueno="luco" fecha="2023-07-02" hora="18:25" sintomas="mocos"/>
-            <Cita mascota="miau" dueno="matute" fecha="2023-08-01" hora="16:55" sintomas ="diarrea"/>
-            <Cita mascota="tiger" dueno="norberto" fecha="2024-09-09" hora="12:15" sintomas="tos"/>
-          </div>
+          <Listado />
         </div>
       </div>
     </>
---
+++ src/components/Listado.jsx
@@ -0,0 +1,12 @@
+import Cita from "./Cita";
+function Listado(){
+    return(
+        <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            <Cita mascota="doggo" dueno="luco" fecha="2023-07-02" hora="18:25" sintomas="mocos"/>
+            <Cita mascota="miau" dueno="matute" fecha="2023-08-01" hora="16:55" sintomas ="diarrea"/>
+            <Cita mascota="tiger" dueno="norberto" fecha="2024-09-09" hora="12:15" sintomas="tos"/>
+        </div>
+    )
+}
+export default Listado;
